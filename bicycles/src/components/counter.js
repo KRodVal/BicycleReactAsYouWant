6952ef78dc7446d1751a96c75f3cd11ae094a493
Kevin R.V <kevinrodriguezvalentin@gmail.com>
2022-04-08 16:43:44 +0100

    Add Button on counter.js

--- bicycles/src/components/counter.js
+++ bicycles/src/components/counter.js
@@ -3,8 +3,16 @@ import { useState } from "react";
 function Counter(){
     const [count, setCount] = useState(0);
 
+    const addOneToCounter = () => {
+        setCount(count + 1);
+    }
+
     return (
-        <p>Counter: {count}</p>
+        <>
+            <p>Counter: {count}</p>
+            <button onClick={addOneToCounter}>Add</button>
+        </>
+        
     );
 }
 
