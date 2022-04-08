ae7783dac006ca13489b95ff7c3b1585e276711f
Kevin R.V <kevinrodriguezvalentin@gmail.com>
2022-04-08 16:38:28 +0100

    counter.js component

--- bicycles/src/pages/helloWorld.js
+++ bicycles/src/pages/helloWorld.js
@@ -1,9 +1,11 @@
 import Footer from '../components/footer'
+import Counter from '../components/counter'
 
 function HelloWorld() {
     return (
         <>
             <p>Hey Smuke Hello World!!!</p>
+            <Counter />
             <Footer />
         </>
         
